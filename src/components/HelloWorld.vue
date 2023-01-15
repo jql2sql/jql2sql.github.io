c1f2fead721f8b5bc27eeb52ee2404192c16fdb0
ByeongKeon Kim <skysign@gmail.com>
2023-01-15 17:52:52 +0900

    Initial deployment with alasql

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup>
-import initSqlJs from 'sql.js';
+import { ref, watch } from 'vue';
+import alasql from 'alasql';
 import Vue3EasyDataTable from 'vue3-easy-data-table';
 import 'vue3-easy-data-table/dist/style.css';
-import { ref, watch } from 'vue'
-import { propsToAttrMap } from '@vue/shared';
+
 
 const props = defineProps({
   msg: String,
@@ -24,33 +24,20 @@ const items = ref([]);
 //   { name: "Kimchi stew", size: "X", price: 10},
 // ]);
 
-const SQL = await initSqlJs({
-  locateFile: () => './node_modules/sql.js/dist/sql-wasm.wasm'
-});
-const db = new SQL.Database();
 let sqlstr = "CREATE TABLE bobs (name char, size char, price int); \
 INSERT INTO bobs VALUES ('Curry', 'XL', 20); \
 INSERT INTO bobs VALUES ('Ramen', 'M', 5); \
 INSERT INTO bobs VALUES ('Hetbahn', 'M', 10); \
 INSERT INTO bobs VALUES ('Kimchi stew', 'X', 10);";
-db.run(sqlstr);
+alasql(sqlstr);
 
 watch(() => props.msg, (newMsg, oldMsg) => {
   try {
     let sqlSelect = `select * from bobs where ${newMsg}`;
-    const rlt = db.exec(sqlSelect);
+    const rlt = alasql(sqlSelect);
+    items.value = rlt;
 
-    items.value = [];
-    for (const row of rlt[0].values) {
-      const json = {}
-      json['name'] = row[0]
-      json['size'] = row[1]
-      json['price'] = row[2]
-      items.value.push(json);
-      
-    }
-    console.log(items.value);
-
+    console.log(rlt);
     console.log('props.msg');
     console.log(props.msg);
     console.log(sqlSelect);
